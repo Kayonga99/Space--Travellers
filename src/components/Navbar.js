eb74f01c29210cb350a13a8e67920cba3383a43e
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-29 22:12:36 +0200

    Update branch.

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import appLogo from '../images/planet.png';
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="header">
+        <div className="logo--header">
+          <img src={appLogo} alt="app logo" />
+          <h1>Space Travellers&apos; Hub</h1>
+        </div>
+        <ul className="nav--all">
+          <li><NavLink className="nav--item" exact to="/">Rockets </NavLink></li>
+          <li><NavLink className="nav--item" to="/missions">Missions </NavLink></li>
+          <li><NavLink className="nav--item" to="/profile">My Profile </NavLink></li>
+        </ul>
+        <Outlet />
+      </nav>
+      <hr />
+    </>
+  );
+}
