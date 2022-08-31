6485e75860c0cbf0bd706a5862ef88e0c62ebd36
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-31 10:18:41 +0200

    Add variables.

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,2 +1,8 @@
 const url = 'https://api.spacexdata.com/v3/rockets';
 
+const GET_ROCKETS = 'GET_ROCKETS';
+const RESERVE_ROCKET = 'RESERVE_ROCKET';
+const CANCEL_REVERATION = 'CANCEL_REVERATION';
+const FETCHING_ROCKETS_FAILED = 'FETCHING_ROCKETS_FAILED';
+
+const initialState = [];
