525d9f6723274442cd3674d230369c432b03355c
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-31 10:23:31 +0200

    Add loadRockets function.

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -5,4 +5,10 @@ const RESERVE_ROCKET = 'RESERVE_ROCKET';
 const CANCEL_REVERATION = 'CANCEL_REVERATION';
 const FETCHING_ROCKETS_FAILED = 'FETCHING_ROCKETS_FAILED';
 
-const initialState = [];
+const initialState = {
+    rockets: []
+};
+
+const loadRockets = (rockets) => ({
+    type: GET_ROCKETS
+})
