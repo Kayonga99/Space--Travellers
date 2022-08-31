6f426a46f743d4f4f79a3f2a067ebd9a19452fef
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-31 15:38:31 +0100

    Set reducers and actions

--- src/components/missions/index.js
+++ src/components/missions/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
-const Missions = () => (
-  <h1>Missions in progress</h1>
-);
+const missions = () => {
+  console.log('Wait');
 
-export default Missions;
+  const BASE_URL = 'https://api.spacexdata.com/v3/missions';
+
+  // const getMissions = async (url) => {
+  //   let data = await fetch(url);
+  //   data = await data.json();
+  //   const missionArray = [];
+  //   data.map((item) => missionArray.push({
+  //     mission_id: item.mission_id,
+  //     mission_name: item.mission_name,
+  //     description: item.description,
+  //   }));
+  //   return missionArray;
+  // };
+
+  const fetchMission = async (url) => {
+    let data = await fetch(url);
+    data = await data.json();
+    const missionArray = [];
+    data.map((item) => missionArray.push({
+      mission_id: item.mission_id,
+      mission_name: item.mission_name,
+      description: item.description,
+    }));
+    return missionArray || [];
+  };
+
+  const getMissionById = async (url, id) => {
+    const missionList = await fetchMission(url);
+    const mission = missionList.find((mission) => mission.mission_id === id);
+    return mission;
+  };
+
+  console.log(getMissionById(BASE_URL, 'F4F83DE'));
+
+  return (
+    <div>missions</div>
+  );
+};
+
+export default missions;
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,6 +1,9 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
 // Action types
-const JOIN = 'spaceTravellers/missions/JOIN/';
-const LEAVE = 'spaceTravellers/missions/LEAVE/';
+const JOIN_MISSION = 'spaceTravellers/missions/JOIN_MISSION/';
+const LEAVE_MISSION = 'spaceTravellers/missions/LEAVE_MISSION/';
+const GET_MISSIONS = 'spaceTravellers/missions/GET_MISSIONS/';
 
 // Initial State
 const initialState = {
@@ -8,30 +11,45 @@ const initialState = {
   status: null,
 };
 
-// const BASE_URL = 'https://api.spacexdata.com/v3/missions';
-
-/*
-Missions:
-mission_id
-mission_name
-description
-*/
+const switchReserved = (state, id) => state.map((mission) => {
+  if (mission.id !== id) { return mission; }
+  return { ...state, reserved: !mission.reserved };
+});
 
 const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case `${JOIN}fulfilled`:
+    case `${JOIN_MISSION}fulfilled`:
       return {
-        missions: action.payload,
+        missions: switchReserved(state, action.payload.id),
         status: 'Mission joined successfully',
       };
-    case `${LEAVE}fulfilled`:
+    case `${LEAVE_MISSION}fulfilled`:
       return {
-        missions: action.payload,
+        missions: switchReserved(state, action.payload.id),
         status: 'Mission left successfully',
       };
     default:
       return state;
   }
 };
+
+const fetchMission = async () => {
+  const BASE_URL = 'https://api.spacexdata.com/v3/missions';
+  let data = await fetch(BASE_URL);
+  data = await data.json();
+  const missionArray = [];
+  data.map((item) => missionArray.push({
+    mission_id: item.mission_id,
+    mission_name: item.mission_name,
+    description: item.description,
+  }));
+  return missionArray || [];
+};
+
+export const getMissionsAction = createAsyncThunk(GET_MISSIONS, async () => fetchMission());
+
+export const joinMissionAction = (id) => ({ type: JOIN_MISSION, payload: id });
+
+export const leaveMissionAction = (id) => ({ type: LEAVE_MISSION, payload: id });
 
 export default missionsReducer;
